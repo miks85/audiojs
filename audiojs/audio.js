60c1dd35294819485c6e59444b00f4269565dd50
miks85 <miks85@users.noreply.github.com>
2014-08-27 14:42:56 +0200

    Update audio.js

--- audiojs/audio.js
+++ audiojs/audio.js
@@ -67,6 +67,8 @@
             <div class="progress"></div> \
             <div class="loaded"></div> \
           </div> \
+            <em class="played">00:00</em>/<strong class="duration">00:00</strong> \ 
+          </div> \
           <div class="error-message"></div>',
         playPauseClass: 'play-pause',
         scrubberClass: 'scrubber',
@@ -74,6 +76,7 @@
         loaderClass: 'loaded',
         timeClass: 'time',
         durationClass: 'duration',
+        playedClass: 'played',
         errorMessageClass: 'error-message',
         playingClass: 'playing',
         loadingClass: 'loading',
